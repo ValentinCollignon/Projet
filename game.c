d9ca6153524d3fec6098cac97f1ac55e5dededff
damienlevy <damienlevy546@gmail.com>
2016-10-06 13:35:53 +0200

    ajout de define

--- game.c
+++ game.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include "game.h"
 
+
+#define SCREEN_H    800 //hauteur de la fenetre
+#define SCREEN_L    600 //largeur de la fenetre
+
+
+//tableau, matrice de bool pour le labyrinthe
+//gestion des affichages?? 
+
 void test(int a)
 {
  printf("%d\n",a);
